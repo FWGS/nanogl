52e0d927c7853d6d5c0a5828d954e1e7754c78ed
mittorn <mittorn@sibmail.com>
2015-06-14 19:57:04 +0700

    Fix hard-float build

--- nanogl.cpp
+++ nanogl.cpp
@@ -72,6 +72,20 @@ static void gl_unimplemented() {
 
 void *nanoGL_GetProcAddress(const char *name)
 {
+#if defined(__MULTITEXTURE_SUPPORT__)
+	if (!strcmp(procname, "glMultiTexCoord2fARB"))
+	{
+		return (void*)&glMultiTexCoord2fARB;
+	}
+	else if (!strcmp(procname, "glActiveTextureARB"))
+	{
+		return (void*)&glActiveTexture;
+	}
+	else if (!strcmp(procname, "glClientActiveTextureARB"))
+	{
+		return (void*)&glClientActiveTexture;
+	}
+#endif
 	return dlsym(glesLib, name);
 }
 
